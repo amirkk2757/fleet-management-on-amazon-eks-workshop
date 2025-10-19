1e25f54c1dd8ad031d6a85df47ca7cdcd202f0bb
amir kamali <amirali.kouhikamali@gevernova.com>
2025-10-18 17:38:43 -0700

    Fix for version v1.0.2

--- terraform/hub/main.tf
+++ terraform/hub/main.tf
@@ -329,13 +329,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.4.0"
 
-  cluster_name                   = local.name
-  cluster_version                = local.cluster_version
-  cluster_endpoint_public_access = true
+  name                   = local.name
+  kubernetes_version                = local.cluster_version
+  endpoint_public_access = true
   authentication_mode            = "API"
 
   # Disabling encryption for workshop purposes
-  cluster_encryption_config = {}
+  encryption_config = {}
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -368,7 +368,7 @@ module "eks" {
   }
 
   # EKS Addons
-  cluster_addons = {
+  addons = {
     coredns    = {
       addon_version  = "v1.11.3-eksbuild.2"
     }
